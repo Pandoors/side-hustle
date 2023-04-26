a467265de98da3ef7bebee58274e238f8ea7c0f3
bartosz <bartoszkruczek@gmail.com>
2023-04-26 23:46:45 +0200

    init commit backend and created frontend rep

---
+++ backend/side-hustle/src/main/java/pl/sidehustle/app/sidehustle/SideHustleApplication.java
@@ -0,0 +1,13 @@
+package pl.sidehustle.app.sidehustle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideHustleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideHustleApplication.class, args);
+    }
+
+}
---
+++ backend/side-hustle/src/test/java/pl/sidehustle/app/sidehustle/SideHustleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sidehustle.app.sidehustle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideHustleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
