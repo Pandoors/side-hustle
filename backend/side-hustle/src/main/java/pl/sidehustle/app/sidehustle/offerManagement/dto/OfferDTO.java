88b0caa0a36e5a1170963c1d8c0336b7d0bfb97a
bartosz <bartoszkruczek@gmail.com>
2023-06-02 01:51:00 +0200

    changes in dtos

--- backend/side-hustle/src/main/java/pl/sidehustle/app/sidehustle/offerManagement/dto/OfferDTO.java
+++ backend/side-hustle/src/main/java/pl/sidehustle/app/sidehustle/offerManagement/dto/OfferDTO.java
@@ -10,6 +10,7 @@ public class OfferDTO {
 
     private Long offerId;
     private String description;
+    private String fullName;
     private String location;
     private String startDate;
     private String endDate;
@@ -17,6 +18,7 @@ public class OfferDTO {
     private String wage;
     private Double longitude;
     private Double latitude;
+    private String city;
 
     public OfferDTO(Long offerId, String description, String location, String startDate, String endDate, String jobType, String formattedDuration, String wage, Double longitude, Double latitude) {
         this.offerId = offerId;
@@ -33,12 +35,14 @@ public class OfferDTO {
     public OfferDTO(Offer offer) {
         this.offerId = offer.getId();
         this.description = offer.getDescription();
-        this.location = offer.getLocation().getCity();
+        this.city = offer.getLocation().getCity();
+        this.location = offer.getLocation().getFullName();
         this.startDate = "";
         this.endDate = "";
         this.jobType = offer.getOfferType();
-        this.wage = offer.getPayment().toString();
+        this.wage = offer.getPayment().toString() + "zł/h";
         this.longitude = offer.getLocation().getLongitude();
         this.latitude = offer.getLocation().getLatitude();
+        this.fullName = offer.getFullName();
     }
 }
